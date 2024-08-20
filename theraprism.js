34b0a5896bb2938a181e79bf36f4faf3f317e2ab
K <redknclarke@gmail.com>
2024-08-19 20:56:49 -0400

    added a ton of symbol substitution ciphers (the amount of images ive downloaded is crazy)

---
+++ theraprism.js
@@ -0,0 +1,39 @@
+const buttons = document.querySelectorAll(".substitution-button");
+const inputArea = document.getElementById("input-area");
+const outputText = document.getElementById("output-text");
+
+function getButtonCharacter(button) {
+    return button.innerHTML.charAt(button.innerHTML.indexOf(".webp") - 1);
+}
+
+for (const button of buttons) {
+    button.addEventListener("click", (e) => {
+        const letter = getButtonCharacter(e.target);
+        outputText.value += letter.toUpperCase();
+        inputArea.appendChild(e.target.children[0].cloneNode(true));
+    });
+}
+
+document.getElementById("backspace-button").addEventListener("click", () => {
+    if (inputArea.lastElementChild) {
+        inputArea.lastElementChild.remove();
+        outputText.value = outputText.value.slice(0, -1);
+    }
+});
+
+document.getElementById("space-button").addEventListener("click", () => {
+    outputText.value += " ";
+    inputArea.innerHTML += `<img src="images/characters/space.png" alt="" class="" />`;
+});
+
+outputText.addEventListener("input", (e) => {
+    inputArea.innerHTML = "";
+    outputText.value = outputText.value.toUpperCase();
+    for (const letter of outputText.value) {
+        if (/^[a-zA-Z]+$/.test(letter)) {
+            inputArea.innerHTML += `<img src="images/characters/theraprism/${letter.toLowerCase()}.webp" alt="" class="" />`;
+        } else if (letter == " ") {
+            inputArea.innerHTML += `<img src="images/characters/space.png" alt="" class="" />`;
+        }
+    }
+});
