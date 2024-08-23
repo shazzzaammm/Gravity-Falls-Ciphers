6688cf44beaea5fc9added61aeacd0b985187f9c
K <redknclarke@gmail.com>
2024-08-23 12:12:30 -0400

    used js es6 modules to make symbol substitution modular

--- combinedSub.js
+++ combinedSub.js
@@ -1,38 +1,7 @@
+import { setup } from "./substitutionCipherModule.js";
+
 const buttons = document.querySelectorAll(".substitution-button");
 const inputArea = document.getElementById("input-area");
 const outputText = document.getElementById("output-text");
 
-function getButtonCharacter(button) {
-    return button.innerHTML.charAt(button.innerHTML.indexOf(".webp") - 1);
-}
-
-for (const button of buttons) {
-    button.addEventListener("click", (e) => {
-        const letter = getButtonCharacter(e.target);
-        outputText.value += letter.toUpperCase();
-        inputArea.appendChild(e.target.children[0].cloneNode(true));
-    });
-}
-
-document.getElementById("backspace-button").addEventListener("click", () => {
-    if (inputArea.lastElementChild) {
-        inputArea.lastElementChild.remove();
-        outputText.value = outputText.value.slice(0, -1);
-    }
-});
-
-document.getElementById("space-button").addEventListener("click", () => {
-    outputText.value += " ";
-    inputArea.innerHTML += `<img src="images/characters/space.png" alt="" class="" />`;
-});
-
-outputText.addEventListener("input", (e) => {
-    inputArea.innerHTML = "";
-    for (const letter of outputText.value) {
-        if (/^[a-zA-Z]+$/.test(letter)) {
-            inputArea.innerHTML += `<img src="images/characters/combined/${letter.toLowerCase()}.webp" alt="" class="" />`;
-        } else if (letter == " ") {
-            inputArea.innerHTML += `<img src="images/characters/space.png" alt="" class="" />`;
-        }
-    }
-});
+setup(buttons, inputArea, outputText, "combined", ".webp")
---
+++ substitutionCipherModule.js
@@ -0,0 +1,36 @@
+export function setup(buttons, inputArea, outputText, imageFolderName, imageExtension) {
+    for (const button of buttons) {
+        button.addEventListener("click", (e) => {
+            const letter = getButtonCharacter(e.target, imageExtension);
+            outputText.value += letter.toUpperCase();
+            inputArea.appendChild(e.target.children[0].cloneNode(true));
+        });
+    }
+
+    document.getElementById("backspace-button").addEventListener("click", () => {
+        if (inputArea.lastElementChild) {
+            inputArea.lastElementChild.remove();
+            outputText.value = outputText.value.slice(0, -1);
+        }
+    });
+
+    document.getElementById("space-button").addEventListener("click", () => {
+        outputText.value += " ";
+        inputArea.innerHTML += `<img src="images/characters/space.png" alt="" class="" />`;
+    });
+
+    outputText.addEventListener("input", (e) => {
+        inputArea.innerHTML = "";
+        for (const letter of outputText.value) {
+            if (/^[a-zA-Z]+$/.test(letter)) {
+                inputArea.innerHTML += `<img src="images/characters/${imageFolderName}/${letter.toLowerCase()}${imageExtension}" alt="" class="" />`;
+            } else if (letter == " ") {
+                inputArea.innerHTML += `<img src="images/characters/space.png" alt="" class="" />`;
+            }
+        }
+    });
+}
+
+function getButtonCharacter(button, extension) {
+    return button.innerHTML.charAt(button.innerHTML.indexOf(extension) - 1);
+}
